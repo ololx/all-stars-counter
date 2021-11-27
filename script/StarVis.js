495dce037c0291cb822f63cf1978d5cf2d5b2ee1
Alexander A. Kropotin <ololx@icloud.com>
2021-11-27 16:36:03 +0500

    fix(StarVis.js) fix stargazers count centering

--- script/StarVis.js
+++ script/StarVis.js
@@ -52,7 +52,7 @@ var StarVis = function(body, painter) {
 			+ encodeURIComponent(`
 				<svg xmlns="http://www.w3.org/2000/svg" width="165" height="120">
 					<polygon points = "90,5 30,120 165,50 15,50 150,120" fill = "yellow"/>
-					<text fill="red" font-size="60" font-family="Times New Roman" x="58" y="90">${count}</text>
+					<text fill="red" font-size="60" font-family="Times New Roman" x="${5 + (165 / 2) - ((29 * `${count}`.length) / 2)}" y="90">${count}</text>
 				</svg>
 			`);
  	}
